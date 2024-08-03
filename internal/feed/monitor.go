871ba619a3f8b4e408f0001f4cf4700c61ab31ce
MrExplode <me@sunstorm.rocks>
2024-08-03 14:06:52 +0200

    Add separate URL for monitor check

--- internal/feed/monitor.go
+++ internal/feed/monitor.go
@@ -9,6 +9,7 @@ import (
 
 type SiteStatusRequest struct {
 	URL           string `yaml:"url"`
+	CheckURL      string `yaml:"check_url"`
 	AllowInsecure bool   `yaml:"allow-insecure"`
 }
 
@@ -20,7 +21,7 @@ type SiteStatus struct {
 }
 
 func getSiteStatusTask(statusRequest *SiteStatusRequest) (SiteStatus, error) {
-	request, err := http.NewRequest(http.MethodGet, statusRequest.URL, nil)
+	request, err := http.NewRequest(http.MethodGet, statusRequest.CheckURL, nil)
 
 	if err != nil {
 		return SiteStatus{
