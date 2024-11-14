a28d847678f146ac2064c2a3dbe2e4cf40db4a15
develoopeer <cooltable01@gmail.com>
2024-11-14 17:47:26 +0300

    Search bar acts like address bar feature implemented

--- internal/assets/static/js/main.js
+++ internal/assets/static/js/main.js
@@ -88,6 +88,16 @@ function updateRelativeTimeForElements(elements)
     }
 }
 
+function isValidUrl(string){
+	let url;
+	try {
+		url = new URL(string);
+	} catch ({ name, message }) {
+		return false;  
+	}
+	return url.protocol === "http:" || url.protocol === "https:";
+}
+
 function setupSearchBoxes() {
     const searchWidgets = document.getElementsByClassName("search");
 
@@ -119,6 +129,10 @@ function setupSearchBoxes() {
 
             if (event.key == "Enter") {
                 const input = inputElement.value.trim();
+				if (isValidUrl(input)) {
+                    window.open(input, '_blank').focus();
+					return;
+				}
                 let query;
                 let searchUrlTemplate;
 
