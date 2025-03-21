b06955e97717824a18a124b36ea7f36668e09cc6
Nionor <36626165+Nionor@users.noreply.github.com>
2025-03-21 08:47:31 +0100

    Added some more constrains

--- internal/glance/static/js/main.js
+++ internal/glance/static/js/main.js
@@ -656,19 +656,28 @@ function setupTruncatedElementTitles() {
 function setupTouch() {
     let touchstartX, touchstartY;
     let touchendX, touchendY;
+    let touchstartT, touchendT;
 
     document.addEventListener('touchstart', function (event) {
         touchstartX = event.changedTouches[0].screenX;
         touchstartY = event.changedTouches[0].screenY;
+        touchstartT = new Date();
     }, false);
 
     document.addEventListener('touchend', function (event) {
         touchendX = event.changedTouches[0].screenX;
         touchendY = event.changedTouches[0].screenY;
+        touchendT = new Date();
+        let touchTime = touchendT - touchstartT;
 
-        const swipeThreshold = 50;
+        const swipeThresholdX = 50; //Min horizontal
+        const swipeThresholdY = 50; //Max vertical 
+        const swipeThresholdT = 800; //Max time
 
-        if (Math.abs(touchendX - touchstartX) > Math.abs(touchendY - touchstartY) && Math.abs(touchendX - touchstartX) > swipeThreshold) {
+        let touchX = Math.abs(touchendX - touchstartX);
+        let touchY = Math.abs(touchendY - touchstartY);
+
+        if (touchX > touchY*2 && touchX > swipeThresholdX && touchY < swipeThresholdY && touchTime < swipeThresholdT) {
             let pos = document.querySelector('input[name="column"]:checked').value;
             let maxCol = document.getElementsByName('column').length-1;
             if (touchendX < touchstartX) {
