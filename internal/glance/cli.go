07e5626469bfab39d7ff20c644fa93060f22a6b1
Svilen Markov <7613769+svilenmarkov@users.noreply.github.com>
2025-04-10 00:39:08 +0100

    Add version & sensors:print cli commands

--- internal/glance/cli.go
+++ internal/glance/cli.go
@@ -5,15 +5,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/shirou/gopsutil/v4/sensors"
 )
 
 type cliIntent uint8
 
 const (
-	cliIntentServe          cliIntent = iota
-	cliIntentConfigValidate           = iota
-	cliIntentConfigPrint              = iota
-	cliIntentDiagnose                 = iota
+	cliIntentVersionPrint cliIntent = iota
+	cliIntentServe
+	cliIntentConfigValidate
+	cliIntentConfigPrint
+	cliIntentDiagnose
+	cliIntentSensorsPrint
 )
 
 type cliOptions struct {
@@ -22,6 +26,15 @@ type cliOptions struct {
 }
 
 func parseCliOptions() (*cliOptions, error) {
+	var args []string
+
+	args = os.Args[1:]
+	if len(args) == 1 && (args[0] == "--version" || args[0] == "-v" || args[0] == "version") {
+		return &cliOptions{
+			intent: cliIntentVersionPrint,
+		}, nil
+	}
+
 	flags := flag.NewFlagSet("", flag.ExitOnError)
 	flags.Usage = func() {
 		fmt.Println("Usage: glance [options] command")
@@ -32,6 +45,7 @@ func parseCliOptions() (*cliOptions, error) {
 		fmt.Println("\nCommands:")
 		fmt.Println("  config:validate     Validate the config file")
 		fmt.Println("  config:print        Print the parsed config file with embedded includes")
+		fmt.Println("  sensors:print       List all sensors")
 		fmt.Println("  diagnose            Run diagnostic checks")
 	}
 	configPath := flags.String("config", "glance.yml", "Set config path")
@@ -41,7 +55,7 @@ func parseCliOptions() (*cliOptions, error) {
 	}
 
 	var intent cliIntent
-	var args = flags.Args()
+	args = flags.Args()
 	unknownCommandErr := fmt.Errorf("unknown command: %s", strings.Join(args, " "))
 
 	if len(args) == 0 {
@@ -51,6 +65,8 @@ func parseCliOptions() (*cliOptions, error) {
 			intent = cliIntentConfigValidate
 		} else if args[0] == "config:print" {
 			intent = cliIntentConfigPrint
+		} else if args[0] == "sensors:print" {
+			intent = cliIntentSensorsPrint
 		} else if args[0] == "diagnose" {
 			intent = cliIntentDiagnose
 		} else {
@@ -64,4 +80,23 @@ func parseCliOptions() (*cliOptions, error) {
 		intent:     intent,
 		configPath: *configPath,
 	}, nil
+}
+
+func cliSensorsPrint() int {
+	tempSensors, err := sensors.SensorsTemperatures()
+	if err != nil {
+		fmt.Printf("Failed to retrieve list of sensors: %v\n", err)
+		return 1
+	}
+
+	if len(tempSensors) == 0 {
+		fmt.Println("No sensors found")
+		return 0
+	}
+
+	for _, sensor := range tempSensors {
+		fmt.Printf("%s: %.1f°C\n", sensor.SensorKey, sensor.Temperature)
+	}
+
+	return 0
 }
